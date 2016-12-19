9fa14b3ad306177d7cb3f390a1396fc3c8fdb181
TOTALTOOL\josh.schubert <josh.schubert@totaltool.com>
2016-12-19 14:40:39 -0600

    Add to file and Export working

--- SPQueryWithIfAndExport.ps1
+++ SPQueryWithIfAndExport.ps1
@@ -1,4 +1,10 @@
-﻿$web = Get-SPWeb http://sharepoint.totaltool.int/sales
+﻿# Setup the correct modules for SharePoint Manipulation 
+if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null ) 
+{     
+   Add-PsSnapin Microsoft.SharePoint.PowerShell 
+} 
+
+$web = Get-SPWeb http://sharepoint.totaltool.int/sales
 $list = $web.Lists["Product Stock Request"]
 
 $spQuery = New-Object Microsoft.SharePoint.SPQuery
@@ -12,28 +18,28 @@ do
     $listItems = $list.GetItems($spQuery)
     $spQuery.ListItemCollectionPosition = $listItems.ListItemCollectionPosition
     foreach($item in $listItems){
+        $Rep = $item["Author"]
+        $PN1 = $item["_x0031__x002e__x0020_Product_x00"]
+        $PN2 = $item["_x0032__x002e__x0020_Product_x00"]
+        $PN3 = $item["_x0033__x002e__x0020_Product_x00"]
+        $Status = $item["Status"]
+        $StartDate = $item["Created"]
         if(($Status -eq "Approved") -and ($StartDate -lt (Get-Date).AddDays(-90))){
-            $Rep = $item["Author"]
-            $PN1 = $item["_x0031__x002e__x0020_Product_x00"]
-            $PN2 = $item["_x0032__x002e__x0020_Product_x00"]
-            $PN3 = $item["_x0033__x002e__x0020_Product_x00"]
-            $Status = $item["Status"]
-            $StartDate = $item["Created"]
-            Write-host $StartDate
-            #$expobj = ""|select Rep, PN1, PN2, PN3, Status, StartDate
-            #$expobj.Rep = $item["Author"]
-            #$expobj.PN1 = $item["_x0031__x002e__x0020_Product_x00"]
-            #$expobj.PN2 = $item["_x0032__x002e__x0020_Product_x00"]
-            #$expobj.PN3 = $item["_x0033__x002e__x0020_Product_x00"]
-            #$expobj.Status = $item["Status"]
-            #$expobj.StartDate = $item["Created"]
+            #Write-host $StartDate, $Status
+            $expobj = ""|select Rep, PN1, PN2, PN3, Status, StartDate
+            $expobj.Rep = $item["Author"]
+            $expobj.PN1 = $item["_x0031__x002e__x0020_Product_x00"]
+            $expobj.PN2 = $item["_x0032__x002e__x0020_Product_x00"]
+            $expobj.PN3 = $item["_x0033__x002e__x0020_Product_x00"]
+            $expobj.Status = $item["Status"]
+            $expobj.StartDate = $item["Created"]
             #Write-Host $expobj
-            #$ExportCollection += $expobj
+            $ExportCollection += $expobj
             }
         
     }
 }
 while ($spQuery.ListItemCollectionPosition -ne $null)
-$filePath = "C:\Scripts\PSR.csv"
+$filePath = "H:\temp\PSR\PSR.csv"
 $ExportCollection |Export-Csv -NoTypeInformation -Path $filePath
 "Exported to: " + $filePath 
